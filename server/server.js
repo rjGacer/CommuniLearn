d8632168b9e40e7dcb0058af6b7f06e914491fa0
rjGacer <raizajonellegacer@gmail.com>
2025-12-17 19:21:53 +0800

    Fix Render port binding

--- server/server.js
+++ server/server.js
@@ -29,7 +29,12 @@ app.get("/", (req, res) => {
   res.send("CommuniLearn API is running");
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 app.use("/students", studentRoutes);
 app.use("/module-comments", moduleComments);
 app.use("/announcement", announcementRoutes);
